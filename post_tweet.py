492b2ce5f330c56b1466dd6b5ae9c442f35a7cb2
abnuo <66645994+abnuo@users.noreply.github.com>
2021-05-03 04:54:15 +0100

    Update post_tweet.py

--- post_tweet.py
+++ post_tweet.py
@@ -1,13 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
 import os
-from twitter_bot_class import TwitterBot
+
+url = 'https://twitter.com/login'
+
+# change this path to where the chromedriver is on your machine
+CHROMEDRIVERPATH = '/Users/rmiriuk/chromedriver'
+
+# change this to your username and password
+userUsername = os.environ['EMAIL']
+userPassword = os.environ['PASSWORD']
+
 
 def tweety(text):
-    try:
-        pj = TwitterBot(os.environ['EMAIL'], os.environ['PASSWORD'])
-        pj.login()
-        pj.post_tweets(text)
-        pj.logout()
-    except Exception as e:
-        pj.logout()
-        print(e)
+    browser = webdriver.Chrome(CHROMEDRIVERPATH)
+    browser.get(url)
 
+    browser.implicitly_wait(5)
+
+    user = browser.find_element_by_name('session[username_or_email]')
+    user.send_keys(userUsername)
+
+    password = browser.find_element_by_name('session[password]')
+    password.send_keys(userPassword)
+    password.send_keys(Keys.ENTER)
+
+    browser.implicitly_wait(5)
+    tweet = browser.find_element_by_css_selector("br[data-text='true']")
+    tweet.send_keys(str(text))
+
+    button = browser.find_element_by_css_selector("div[data-testid='tweetButtonInline']")
+    button.click()
+
+    browser.close()
